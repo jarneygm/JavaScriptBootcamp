d5128ece5b92d608221e46c7a5a59c9f293b6def
JavierPaternina <javiereduardopaternina@gmail.com>
2015-02-28 17:10:38 -0500

    new functions controller and call the api

--- App/Controller/MoviesController.js
+++ App/Controller/MoviesController.js
@@ -1,35 +1,79 @@
-        (function () {
+            (function () {
 
-                var apiConfigPath = {};
-                var dashboardList = {
-                    nowPlay:{},
-                    upComing:{}
-                };
-                var App = angular.module("Movies.Controller",[]);
-
-                    App.controller('LoginApiCtrl',['ConfigApiService',function(ConfigApiService){
-                         ConfigApiService.getConfigPath().then(function(data){
-
-                             if(data.isValid)
-                                apiConfigPath = data.data;
-                        });
-
-                     }]);
-
-                    App.controller('MovieListCtrl',['MovieDiscover',function(MovieDiscover){
-
-                         MovieDiscover.getNowPlaying().then(function(data){
-                             dashboardList.nowPlay = data.data;
-                         });
-
-                         MovieDiscover.getUpcomingMovies().then(function(data){
-                             dashboardList.upComing = data.data;
-                         });
+                    var apiConfigPath = {};
 
 
-                         console.log(dashboardList);
+                    var App = angular.module("Movies.Controller",[]);
 
-                    }]);
+                        App.controller('LoginApiCtrl',['ConfigApiService',function(ConfigApiService){
+                             ConfigApiService.getConfigPath().then(function(data){
+
+                                 if(data.isValid)
+                                    apiConfigPath = data.data;
+                            });
+
+                         }]);
+
+                        App.controller('MovieListCtrl',['$scope','MovieDiscover',function($scope,MovieDiscover){
+
+                            var self = this;
+                            self.mostRate = {};
+                            self.upComingMovies = [];
+                            self.nowPlayingMovies = {};
+                            self.urlImag = '';
+                            var rank = 0;
+                            var movieId = null;
+                             self.getListMovies  = function(){
+
+                                 MovieDiscover.getUpcomingMovies().then(function(response){
+                                     self.upComingMovies = response.results;
+
+                                    getOutNullValue(self.upComingMovies);
+                                    angular.forEach(self.upComingMovies,function(val,key){
+
+                                            if(rank <= val.vote_average){
+                                                movieId = val.id;
+                                                rank = val.vote_average;
+                                            }
+
+                                     });
+
+                                     console.log(self.upComingMovies);
+                                     MovieDiscover.getMovieById(movieId).then(function(resp){
+
+                                         resp.poster_path = apiConfigPath.images.base_url + apiConfigPath.images.poster_sizes[4] + resp.poster_path;
+                                         self.mostRate = resp;
+                                     });
+                                     self.urlImag =  apiConfigPath.images.base_url + apiConfigPath.images.poster_sizes[1];
+
+                                 });
+
+                                /* MovieDiscover.getNowPlaying().then(function(response){
+                                     self.nowPlayingMovies = response;
+
+                                 });*/
+
+                             };
+
+                            function getOutNullValue(list){
+
+                                angular.forEach(list,function(val,key){
+                                    if(val.poster_path === null){
+                                        list.splice(list.indexOf(key),1);
+                                    }
+
+                                });
+
+                            }
+
+                            self.getListMovies();
+
+
+
+
+
+
+                        }]);
 
 
 
@@ -37,4 +81,4 @@
 
 
 
-        })();
+            })();
--- App/Model/MoviesModel.js
+++ App/Model/MoviesModel.js
@@ -1,108 +1,123 @@
-    (function(){
+        (function(){
 
-        var config = {
+            var config = {
 
-            api_key : "070d248d81c80314c37280957276a5d4"
-
-        };
-
-        var UrlBase = {
-
-            httpBaseApi:'http://api.themoviedb.org/3/'
-        };
-
-        var ApiUrl = {
-
-            configuration : UrlBase.httpBaseApi + 'configuration',
-            discoverMovie: UrlBase.httpBaseApi + 'discover/movie',
-            upcomingMovies : UrlBase.httpBaseApi + 'movie/upcoming',
-            nowPlaying :UrlBase.httpBaseApi + 'movie/now_playing'
-        };
-
-        var App = angular.module('Movies.Models',[]);
-
-
-        App.factory('ConfigApiService',['$http','$q',function($http,$q){
-
-            var configData = function(){
-                var configApi, def;
-                configApi = {};
-                def = $q.defer();
-                $http.get(ApiUrl.configuration,{params:{api_key:config.api_key}}).success(function(data){
-
-                    configApi.isValid = true;
-                    configApi.data = data;
-                    def.resolve(configApi);
-
-                }).error(function(data){
-                    def.reject(data);
-                });
-               return  def.promise;
-            };
-            return {
-                getConfigPath : configData
-            };
-
-
-        }]);
-
-       App.factory('MovieDiscover',['$http','$q',function($http,$q){
-
-            var modelData = {
-                    isValid:false,
-                    data:{},
-                    errorMsg:''
+                api_key : "070d248d81c80314c37280957276a5d4"
 
             };
 
-             var defer = $q.defer();
+            var UrlBase = {
 
-             // Private Method
-             var discoverMovie = function(){
-
-                 $http.get(ApiUrl.discoverMovie,{params:{api_key:config.api_key}}).success(Response).error(Error);
-                 return defer.promise;
-             };
-
-             var  upComingMovies = function(){
-
-                 $http.get(ApiUrl.upcomingMovies,{params:{api_key:config.api_key}}).success(Response).error(Error);
-                 return defer.promise;
-
-             };
-
-             var nowPlayong = function(){
-
-                 $http.get(ApiUrl.nowPlaying,{params:{api_key:config.api_key}}).success(Response).error(Error);
-                 return defer.promise;
-             };
-
-             function Error(data){
-
-                 modelData.errorMsg='Something Wrong happens';
-                 modelData.data = data;
-                 defer.reject(modelData);
-             }
-
-             function Response(data){
-
-                 modelData.isValid = true;
-                 modelData.data = data;
-                 defer.resolve(modelData);
-             }
-
-            return {
-
-                getMovieList: discoverMovie,
-                getUpcomingMovies:upComingMovies,
-                getNowPlaying : nowPlayong
-
+                httpBaseApi:'http://api.themoviedb.org/3/'
             };
 
-        }]);
+            var ApiUrl = {
+
+                configuration : UrlBase.httpBaseApi + 'configuration',
+                discoverMovie: UrlBase.httpBaseApi + 'discover/movie',
+                upcomingMovies : UrlBase.httpBaseApi + 'movie/upcoming',
+                nowPlaying :UrlBase.httpBaseApi + 'movie/now_playing',
+                movieDescription: UrlBase.httpBaseApi + 'movie/'
+            };
+
+            var App = angular.module('Movies.Models',[]);
+
+
+            App.factory('ConfigApiService',['$http','$q',function($http,$q){
+
+                var configData = function(){
+                    var configApi, def;
+                    configApi = {};
+                    def = $q.defer();
+                    $http.get(ApiUrl.configuration,{params:{api_key:config.api_key}}).success(function(data){
+
+                        configApi.isValid = true;
+                        configApi.data = data;
+                        def.resolve(configApi);
+
+                    }).error(function(data){
+                        def.reject(data);
+                    });
+                   return  def.promise;
+                };
+                return {
+                    getConfigPath : configData
+                };
+
+
+            }]);
+
+           App.factory('MovieDiscover',['$http','$q',function($http,$q){
+
+                var modelData = {
+                        isValid:false,
+                        data:{},
+                        errorMsg:''
+
+                }, deferNowPlaying = $q.defer(),deferUpcoming = $q.defer(),deferDescription = $q.defer();
+
+
+                 // Private Method
+                 var discoverMovie = function(){
+
+                     $http.get(ApiUrl.discoverMovie,{params:{api_key:config.api_key}}).success(ResponseUpComing).error(Error);
+                     return deferUpcoming.promise;
+                 };
+
+                 var  upComingMovies = function(){
+
+                     $http.get(ApiUrl.upcomingMovies,{params:{api_key:config.api_key}}).success(ResponseUpComing).error(Error);
+                     return deferUpcoming.promise;
+
+                 };
+
+                 var nowPlaying = function(){
+
+                     $http.get(ApiUrl.nowPlaying,{params:{api_key:config.api_key}}).success(ResponseNowPlaying).error(Error);
+                     return deferNowPlaying.promise;
+                 };
+
+                 var movieDescription = function(movieId){
+
+                      $http.get(ApiUrl.movieDescription + movieId,{params:{api_key:config.api_key}}).success(ResponseMovieDescription).error(Error);
+                      return deferDescription.promise;
+
+                  };
+                 function Error(data){
+
+                     deferUpcoming.reject(data);
+                 }
+
+                 function ResponseUpComing(data){
+
+                     deferUpcoming.resolve(data);
+                 }
+
+
+                  function ResponseNowPlaying(data){
+
+                    deferNowPlaying.resolve(data);
+                  }
+
+                  function ResponseMovieDescription(data){
+
+                      deferDescription.resolve(data);
+
+                  }
+
+                return {
+
+                    getMovieList: discoverMovie,
+                    getUpcomingMovies:upComingMovies,
+                    getNowPlaying : nowPlaying,
+                    getMovieById :movieDescription
+
+                };
+
+            }]);
 
 
 
 
 
-    })();
+        })();
