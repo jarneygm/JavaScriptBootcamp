c6de55c15b2c3f92b0bfd7e0c0416db28734cf5c
Jhon Guerrero <jarneygm@gmail.com>
2015-03-10 17:02:22 -0500

    Chak-point martes.
    Faltas algunas caracteristicas.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,105 @@
 ﻿$(document).ready(function () {
 
-    var apiConfiguration;
+    //var apiConfiguration;
 
-    // get basic configuration from API
-    $.ajax({
-        "url": "http://api.themoviedb.org/3/configuration?api_key=070d248d81c80314c37280957276a5d4",
-        "type": "GET",
-        "async": false,
-        "success": function (responseFromServer) {
-            apiConfiguration = responseFromServer;
-        }
-    });
+    //// get basic configuration from API
+    //$.ajax({
+    //    "url": "http://api.themoviedb.org/3/configuration?api_key=070d248d81c80314c37280957276a5d4",
+    //    "type": "GET",
+    //    "async": false,
+    //    "success": function (responseFromServer) {
+    //        apiConfiguration = responseFromServer;
+    //    }
+    //});
 
     // make sure you get the configuration from API
-    if (!apiConfiguration) return;
+    if (!movieApi.isValid) return;
 
-    var theMovieDBUri = "http://api.themoviedb.org/3/";
-    var apiKey = "070d248d81c80314c37280957276a5d4";
+    //var theMovieDBUri = "http://api.themoviedb.org/3/";
+    //var apiKey = "070d248d81c80314c37280957276a5d4";
 
-    // load popular movies
-    $.get(theMovieDBUri + "movie/popular",
-        { "api_key": apiKey },
+    //// load popular movies
+    //$.get("http://api.themoviedb.org/3/movie/popular?api_key=070d248d81c80314c37280957276a5d4",
+    //    function (dataFromServer) {
+    //        // walk throw all movies
+    //        for (var key in dataFromServer.results) {
+    //            // building the movies and add it to the DOM
+    //            var template = "<div class=\"col-lg-2 col-md-3 col-sm-3 col-xs-10 movie-gadget\">" +
+    //                "<div class=\"row no-padder\">" +
+    //                    "<a href=\"#\"><img class=\"img-gadget\" src=\"" + movieApi.configuration.images.base_url + movieApi.configuration.images.poster_sizes[3] + dataFromServer.results[key].poster_path + "\" /></a>" +
+    //                "</div>" +
+    //                "<div class=\"row gadget-content no-padder\">" +
+    //                    "<label style=\"margin-top:10px\">" + dataFromServer.results[key].original_title + "</label>" +
+    //                "</div></div>";
+    //            $("#popularMovies").append(template);
+    //        }
+    //    });
+
+    //$.get(theMovieDBUri + "movie/now_playing",
+    //    { "api_key": apiKey },
+    //    function (dataFromServer) {
+    //        // walk throw all movies
+    //        for (var i = 0; i < dataFromServer.results.length; i++) {
+    //            // building the movies and add it to the DOM
+    //            var movie = new Movie(dataFromServer.results[i], movieApi.configuration);
+    //            $("#nowPlayingMovies").append(movie.template);
+    //        }
+    //    });
+
+    //var getMovie = function (category, selector) {
+    //    $.get(category,
+    //    { "api_key": movieApi.apiKey },
+    //    function (dataFromServer) {
+    //        renderMovies(dataFromServer, selector);
+    //    });
+    //};
+
+    var getMovie = function (category, selector) {
+        $.get(category,
+        { "api_key": movieApi.apiKey },
         function (dataFromServer) {
-            // we decide here what to do with the data
-            for (var key in dataFromServer.results) {
-                var template = "<div class=\"col-lg-2 col-md-3 col-sm-3 col-xs-10 movie-gadget\">" +
-                    "<div class=\"row no-padder\">" +
-                        "<a href=\"#\"><img class=\"img-gadget\" src=\"" + apiConfiguration.images.base_url + apiConfiguration.images.poster_sizes[3] + dataFromServer.results[key].poster_path + "\" /></a>" +
-                    "</div>" +
-                    "<div class=\"row gadget-content no-padder\">" +
-                        "<label style=\"margin-top:10px\">" + dataFromServer.results[key].original_title + "</label>" +
-                    "</div></div>";
-                $("#popularMovies").append(template);
+            // walk throw all movies
+            for (var i = 0; i < dataFromServer.results.length; i++) {
+                // building the movies and add it to the DOM
+                var movie = new Movie(dataFromServer.results[i], movieApi.configuration);
+                $(selector).append(movie.template);
             }
         });
+    };
+
+    getMovie(movieApi.upcomingMovies, "#upcomingMovies");
+    //$.get(movieApi.upcomingMovies,
+    //    { "api_key": movieApi.apiKey },
+    //    function (dataFromServer) {
+    //        renderMovies(dataFromServer, "#upcomingMovies");
+    //    });
+
+    getMovie(movieApi.nowPlaying, "#nowPlayingMovies");
+    //$.get(movieApi.nowPlaying,
+    //    { "api_key": movieApi.apiKey },
+    //    function (dataFromServer) {
+    //        renderMovies(dataFromServer, "#nowPlayingMovies");
+    //    });
+
+    getMovie(movieApi.popularMovies, "#popularMovies");
+    //$.get(movieApi.popularMovies,
+    //    { "api_key": movieApi.apiKey },
+    //    function (dataFromServer) {
+    //        renderMovies(dataFromServer, "#popularMovies");
+    //    });
+
+    //var renderMovies = function (dataFromServer, selector) {
+    //    // walk throw all movies
+    //    for (var i = 0; i < dataFromServer.results.length; i++) {
+    //        // building the movies and add it to the DOM
+    //        var movie = new Movie(dataFromServer.results[i], movieApi.configuration);
+    //        $(selector).append(movie.template);
+    //    }
+    //};
+
+    $(".popup").click(function (event) {
+        event.preventDefault();
+        alert($(this).data("movieid"));
+        $("#myModal").show();
+    })
 });
---
+++ js/settings.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    movieApi = { "isValid": false };
+
+    // get basic configuration from API
+    $.ajax({
+        "url": "http://api.themoviedb.org/3/configuration?api_key=070d248d81c80314c37280957276a5d4",
+        "type": "GET",
+        "async": false,
+        "success": function (responseFromServer) {
+            movieApi.configuration = responseFromServer;
+            movieApi.isValid = true;
+        }
+    });
+
+    movieApi.theMovieDBUri = "http://api.themoviedb.org/3/";
+    movieApi.apiKey = "070d248d81c80314c37280957276a5d4";
+    movieApi.upcomingMovies = movieApi.theMovieDBUri + "movie/upcoming";
+    movieApi.nowPlaying = movieApi.theMovieDBUri + "movie/now_playing";
+    movieApi.popularMovies = movieApi.theMovieDBUri + "movie/popular";
+})();
